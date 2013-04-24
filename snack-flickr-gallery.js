c35375d103d361a2054d83eb9b57885497bb8961
John Barton (joho) <jrbarton@gmail.com>
2013-04-24 22:00:22 +1000

    That appears to work just fine.

--- snack-flickr-gallery.js
+++ snack-flickr-gallery.js
@@ -8,7 +8,6 @@
     var galleryDivs = snack.wrap('.snack-flickr-gallery');
 
     galleryDivs.each(function (element) {
-      var wrappedElement = snack.wrap(element);
       var params = { format: "json" }; // always set format or we get xml back :(
 
       // testing options
@@ -23,40 +22,53 @@
 
         snack.each(response.items, function (item) {
           var newImage = document.createElement("img");
-          newImage["src"] = item.media.m;
+          newImage["src"] = item.media.m.replace("_m.", ".");
           newImage["alt"] = item.title;
-          if (!firstImageInserted) {
-            snack.wrap(newImage).addClass("active")
-          }
 
           element.appendChild(newImage);
-        });
-      });
 
-      // set up the animation delay
-      var slideshowRotation = setInterval(function () {
-        console.log("should transition image now");
-        var images, currentImage, nextImage;
-
-        images = element.getElementsByTagName('img');
-        for(var i = 0; i < images.length; i++) {
-          var currentImage = images[i];
-          
-          if (currentImage.className.indexOf('active') > -1) {
-            // figure out next image
-            var nextImage = images[i + 1];
-            if (typeof(nextImage) === 'undefined') {
-              nextImage = images[0];
-            }
-            
-            // switch classes
-            snack.wrap(nextImage).addClass('active');
-            snack.wrap(currentImage).removeClass('active');
+          if (!firstImageInserted) {
+            snack.wrap(newImage).addClass("active");
+            firstImageInserted = true;
           }
-        }
+        });
 
-        // clearInterval(slideshowRotation);
-      }, 3000);
+        // var anyImagesCurrentlyActive = false;
+        // set up the animation delay
+        var slideshowRotation = setInterval(function () {
+          console.log("should transition image now");
+          var images, currentImage, nextImage;
+
+          images = element.getElementsByTagName('img');
+          for(var i = 0; i < images.length; i++) {
+            currentImage = images[i];
+            
+            if (currentImage.className.indexOf('active') > -1) {
+              console.log("Found active image at index " + i);
+              // figure out next image
+              nextImage = images[i + 1];
+              if (typeof(nextImage) === 'undefined') {
+                nextImage = images[0];
+              }
+              
+              // switch classes
+              snack.wrap(nextImage).addClass('active');
+              snack.wrap(currentImage).removeClass('active');
+              break;
+            }
+          }
+
+          // if (!anyImagesCurrentlyActive) {
+          //   console.log("setting first image active");
+          //   snack.wrap(images[0]).addClass("active");
+          //   anyImagesCurrentlyActive = true;
+          // }
+
+          // clearInterval(slideshowRotation);
+        }, 3000);
+
+      }); // end jsonp
+
 
     });
 
